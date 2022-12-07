d84f6aa52c83438d416ae76b76865c34eedde55f
woojeong <woojeong@c5r3s3.42seoul.kr>
2022-12-07 21:48:12 +0900

    norm....

--- srcs/fill_cmd_info.c
+++ srcs/fill_cmd_info.c
@@ -100,23 +100,25 @@ char	*replace_symbol_to_text(char *str, t_var_lst *env_lst)
 
 int	fill_cmd_info_arr(t_cmd_info *cmd_info_arr, t_list *token_list, t_list *tmp_list, t_var_lst *env_lst)
 {
-	t_list	*token_cur;
-	t_list	*tmp_cur;
+	t_list	*cur_token;
+	t_list	*cur_tmp;
 	int		cmd_i;
 	int		av_i;
 	int		re_i;
 	int		here_i;
 
-	token_cur = token_list;
-	tmp_cur = tmp_list;
+	cur_token = token_list;
+	cur_tmp = tmp_list;
 	av_i = 0;
 	re_i = 0;
 	cmd_i = 0;
 	here_i = 0;
 	// 파이프 마지막 전까지의 cmd_info
-	while (token_cur && token_cur -> content) {
-		if (!ft_strncmp(token_cur -> content, "|", 2)) {
-			if (!token_cur->next)
+	while (cur_token)
+	{
+		if (!ft_strncmp(cur_token -> content, "|", 2))
+		{
+			if (!cur_token->next)
 			{
 				token_err("newline", NULL);
 				return (1);
@@ -126,23 +128,22 @@ int	fill_cmd_info_arr(t_cmd_info *cmd_info_arr, t_list *token_list, t_list *tmp_
 			here_i = 0;
 			cmd_i += 1;
 		}
-		else if (is_redir(token_cur -> content)) {
-			cmd_info_arr[cmd_i].redir[re_i].type = get_type(token_cur->content);
-			if (token_cur->next)
+		else if (is_redir(cur_token -> content))
+		{
+			cmd_info_arr[cmd_i].redir[re_i].type = get_type(cur_token->content);
+			if (cur_token->next)
 			{
-				token_cur = token_cur -> next;
+				cur_token = cur_token -> next;
 				if (cmd_info_arr[cmd_i].redir[re_i].type == HERE_DOC)
 				{
 					cmd_info_arr[cmd_i].redir[re_i].type = INFILE;
-					cmd_info_arr[cmd_i].redir[re_i].str = ft_strdup(tmp_cur->content);
-					cmd_info_arr[cmd_i].here[here_i] = ft_strdup(tmp_cur->content);
-					tmp_cur = tmp_cur->next;
+					cmd_info_arr[cmd_i].redir[re_i].str = ft_strdup(cur_tmp->content);
+					cmd_info_arr[cmd_i].here[here_i] = ft_strdup(cur_tmp->content);
+					cur_tmp = cur_tmp->next;
 					here_i += 1;
 				}
 				else
-				{
-					cmd_info_arr[cmd_i].redir[re_i].str = replace_symbol_to_text(token_cur->content, env_lst);
-				}
+					cmd_info_arr[cmd_i].redir[re_i].str = replace_symbol_to_text(cur_token->content, env_lst);
 				re_i += 1;
 			}
 			else
@@ -151,11 +152,12 @@ int	fill_cmd_info_arr(t_cmd_info *cmd_info_arr, t_list *token_list, t_list *tmp_
 				return (1);
 			}
 		}
-		else {
-			cmd_info_arr[cmd_i].argv[av_i] = replace_symbol_to_text(token_cur->content, env_lst);
+		else
+		{
+			cmd_info_arr[cmd_i].argv[av_i] = replace_symbol_to_text(cur_token->content, env_lst);
 			av_i += 1;
 		}
-		token_cur = token_cur -> next;
+		cur_token = cur_token -> next;
 	}
 	return (0);
 }
--- srcs/init_cmd_info.c
+++ srcs/init_cmd_info.c
@@ -40,20 +40,33 @@ void	init_cmd_info_val(t_cmd_info *cmd_info_arr, int pipe_num)
 	}
 }
 
-int	init_cmd_info(t_cmd_info *cmd_info, int *cnt)
+int	init_cmd_argv(t_cmd_info *cmd_info, int *cnt)
 {
 	int	i;
 
+	i = 0;
 	cmd_info->argc = cnt[ARGC];
 	cmd_info->argv = malloc(sizeof(char *) * (cnt[ARGC] + 1));
+	if (!cmd_info->argv)
+		return (0);
 	i = 0;
 	while (i < cnt[ARGC] + 1)
 	{
 		cmd_info->argv[i] = NULL;
 		i++;
 	}
+	return (1);
+}
+
+int	init_cmd_redir(t_cmd_info *cmd_info, int *cnt)
+{
+	int	i;
+
+	i = 0;
 	cmd_info->redir_num = cnt[REDIR];
 	cmd_info->redir = malloc(sizeof(t_redir) * (cnt[REDIR] + 1));
+	if (!cmd_info->redir)
+		return (1);
 	i = 0;
 	while (i < cnt[REDIR] + 1)
 	{
@@ -61,15 +74,35 @@ int	init_cmd_info(t_cmd_info *cmd_info, int *cnt)
 		cmd_info->redir[i].str = NULL;
 		i++;
 	}
+	return (1);
+}
+
+int init_cmd_here(t_cmd_info *cmd_info, int *cnt)
+{
+	int	i;
+
+	i = 0;
 	cmd_info->here_num = cnt[HERE];
 	cmd_info->here = malloc(sizeof(char *) * (cnt[HERE] + 1));
+	if (cmd_info->here)
+		return (0);
 	i = 0;
 	while (i < cnt[HERE] + 1)
 	{
 		cmd_info->here[i] = NULL;
 		i++;
 	}
-	// malloc err 체크
+	return (1);
+}
+
+int	init_cmd_info(t_cmd_info *cmd_info, int *cnt)
+{
+	if (!init_cmd_argv(cmd_info, cnt))
+		return (0);
+	if (!init_cmd_redir(cmd_info, cnt))
+		return (0);
+	if (!init_cmd_here(cmd_info, cnt))
+		return (0);
 	return (1);
 }
 
@@ -81,6 +114,8 @@ t_cmd_info	*init_cmd_info_arr(t_list *token_list, int pipe_num, t_list **here_li
 	int			cnt[3];
 
 	cmd_info_arr = malloc(sizeof(t_cmd_info) * (pipe_num + 1)); //malloc
+	if (!cmd_info_arr)
+		return (NULL);
 	init_cmd_info_val(cmd_info_arr, pipe_num);
 	cnt[ARGC] = 0;
 	cnt[REDIR] = 0;
@@ -88,21 +123,15 @@ t_cmd_info	*init_cmd_info_arr(t_list *token_list, int pipe_num, t_list **here_li
 	cur = token_list;
 	cmd_i = 0;
 	// 파이프 마지막 전까지의 cmd_info를 malloc
-	while (cur) {
-		if (!ft_strncmp(cur -> content, "|", 2)) {
-			// 파이프로 끝난건 나중에
-			if (!cnt[ARGC] && !cnt[REDIR] && !cnt[HERE])
+	while (cur)
+	{
+		if (!ft_strncmp(cur -> content, "|", 2))
+		{
+			if (!cnt[ARGC] && !cnt[REDIR])
 			{
 				token_err(cur -> content, syntax_err);
-				free(cur->content);
-				cur->content = NULL;
-				break ;
+				return (cmd_info_arr);
 			}
-			// if (cur->next && !ft_strncmp(cur->next->content, "|", 2))
-			// {
-			// 	token_err(cur->next->content);
-			// 	break;
-			// }
 			init_cmd_info(cmd_info_arr + cmd_i, cnt);
 			printf("[argc: %d, redir: %d]\n", cnt[ARGC], cnt[REDIR]);
 			cnt[ARGC] = 0;
@@ -110,19 +139,18 @@ t_cmd_info	*init_cmd_info_arr(t_list *token_list, int pipe_num, t_list **here_li
 			cnt[HERE] = 0;
 			cmd_i++;
 		}
-		else if (is_redir(cur -> content)) {
+		else if (is_redir(cur -> content))
+		{
 			if (cur->next && (is_redir(cur->next->content) || !ft_strncmp(cur->next->content, "|", 2)))
 			{
 				token_err(cur->next->content, syntax_err);
-				free(cur->content);
-				cur->content = NULL;
-				break;
+				return (cmd_info_arr);
 			}
 			cnt[REDIR] += 1;
-			if (!ft_strncmp(cur -> content, "<<", 3) && cur->next) {
+			if (!ft_strncmp(cur -> content, "<<", 3) && cur->next)
+			{
 				cnt[HERE] += 1;
 				ft_lstadd_back(here_list, ft_lstnew(ft_strdup(cur->next->content)));
-				// append delim to here_list
 			}
 			if (cur->next)
 				cur = cur -> next;
